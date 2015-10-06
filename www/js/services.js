267f8ba1e5cbcc86b8619b22fd764e624dcdf003
Kirill Gusyatin <kirill.gusyatin@ekreative.com>
2015-10-06 16:06:51 +0300

    API-service creating

--- www/js/services.js
+++ www/js/services.js
@@ -1,50 +1,114 @@
 angular.module('starter.services', [])
+    .factory('API', function ($http, $q, config) {
+        return {
+            get: function (url, headers) {
+                var deferred = $q.defer(),
+                    configObj = {
+                        headers: headers || {}
+                    };
 
-.factory('Chats', function() {
-  // Might use a resource here that returns a JSON array
+                $http.get(config.apiServer + url, configObj).then(function (data) {
+                    deferred.resolve(data);
+                }, function (err) {
+                    deferred.reject(err);
+                });
 
-  // Some fake testing data
-  var chats = [{
-    id: 0,
-    name: 'Ben Sparrow',
-    lastText: 'You on your way?',
-    face: 'https://pbs.twimg.com/profile_images/514549811765211136/9SgAuHeY.png'
-  }, {
-    id: 1,
-    name: 'Max Lynx',
-    lastText: 'Hey, it\'s me',
-    face: 'https://avatars3.githubusercontent.com/u/11214?v=3&s=460'
-  }, {
-    id: 2,
-    name: 'Adam Bradleyson',
-    lastText: 'I should buy a boat',
-    face: 'https://pbs.twimg.com/profile_images/479090794058379264/84TKj_qa.jpeg'
-  }, {
-    id: 3,
-    name: 'Perry Governor',
-    lastText: 'Look at my mukluks!',
-    face: 'https://pbs.twimg.com/profile_images/598205061232103424/3j5HUXMY.png'
-  }, {
-    id: 4,
-    name: 'Mike Harrington',
-    lastText: 'This is wicked good ice cream.',
-    face: 'https://pbs.twimg.com/profile_images/578237281384841216/R3ae1n61.png'
-  }];
+                return deferred.promise;
+            },
 
-  return {
-    all: function() {
-      return chats;
-    },
-    remove: function(chat) {
-      chats.splice(chats.indexOf(chat), 1);
-    },
-    get: function(chatId) {
-      for (var i = 0; i < chats.length; i++) {
-        if (chats[i].id === parseInt(chatId)) {
-          return chats[i];
+            post: function (url, data, headers) {
+                var deferred = $q.defer(),
+                    configObj = {
+                        headers: headers || {}
+                    };
+
+                $http.post(config.apiServer + url, data, configObj).then(function (result) {
+                    deferred.resolve(result);
+                }, function (err) {
+                    deferred.reject(err);
+                });
+
+                return deferred.promise;
+            },
+
+            put: function (url, data, headers) {
+                var deferred = $q.defer(),
+                    configObj = {
+                        headers: headers || {}
+                    };
+
+                $http.put(config.apiServer + url, data, configObj).then(function (result) {
+                    deferred.resolve(result);
+                }, function (err) {
+                    deferred.reject(err);
+                });
+
+                return deferred.promise;
+            },
+
+            delete: function (url, headers) {
+                var deferred = $q.defer(),
+                    configObj = {
+                        headers: headers || {}
+                    };
+
+                $http.delete(config.apiServer + url, configObj).then(function (result) {
+                    deferred.resolve(result);
+                }, function (err) {
+                    deferred.reject(err);
+                });
+
+                return deferred.promise;
+            }
+
         }
-      }
-      return null;
-    }
-  };
-});
+    })
+
+  .factory('Chats', function () {
+      // Might use a resource here that returns a JSON array
+
+      // Some fake testing data
+      var chats = [{
+          id: 0,
+          name: 'Ben Sparrow',
+          lastText: 'You on your way?',
+          face: 'https://pbs.twimg.com/profile_images/514549811765211136/9SgAuHeY.png'
+      }, {
+          id: 1,
+          name: 'Max Lynx',
+          lastText: 'Hey, it\'s me',
+          face: 'https://avatars3.githubusercontent.com/u/11214?v=3&s=460'
+      }, {
+          id: 2,
+          name: 'Adam Bradleyson',
+          lastText: 'I should buy a boat',
+          face: 'https://pbs.twimg.com/profile_images/479090794058379264/84TKj_qa.jpeg'
+      }, {
+          id: 3,
+          name: 'Perry Governor',
+          lastText: 'Look at my mukluks!',
+          face: 'https://pbs.twimg.com/profile_images/598205061232103424/3j5HUXMY.png'
+      }, {
+          id: 4,
+          name: 'Mike Harrington',
+          lastText: 'This is wicked good ice cream.',
+          face: 'https://pbs.twimg.com/profile_images/578237281384841216/R3ae1n61.png'
+      }];
+
+      return {
+          all: function () {
+             return chats;
+          },
+          remove: function (chat) {
+             chats.splice(chats.indexOf(chat), 1);
+          },
+          get: function (chatId) {
+              for (var i = 0; i < chats.length; i++) {
+                  if (chats[i].id === parseInt(chatId)) {
+                      return chats[i];
+                  }
+              }
+              return null;
+          }
+      };
+  });
