f169fbb44b2358e09f71349ed233e3d775060896
Kirill Gusyatin <kirill.gusyatin@ekreative.com>
2015-10-08 10:00:31 +0300

    Scripts gulp task in 'ionic serve'

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,23 @@
-var gulp      = require('gulp'),
-    gutil     = require('gulp-util'),
-    bower     = require('bower'),
-    concat    = require('gulp-concat'),
-    sass      = require('gulp-sass'),
-    minifyCss = require('gulp-minify-css'),
-    imagemin  = require('gulp-imagemin'),
-    rename    = require('gulp-rename'),
-    sh        = require('shelljs');
+var gulp       = require('gulp'),
+    gutil      = require('gulp-util'),
+    bower      = require('bower'),
+    concat     = require('gulp-concat'),
+    sass       = require('gulp-sass'),
+    minifyCss  = require('gulp-minify-css'),
+    imagemin   = require('gulp-imagemin'),
+    rename     = require('gulp-rename'),
+    sourcemaps = require('gulp-sourcemaps'),
+    babel      = require('gulp-babel'),
+    ngAnnotate = require('gulp-ng-annotate'),
+    uglify     = require('gulp-uglify'),
+    sh         = require('shelljs');
 
 
-gulp.task('default', ['sass', 'images']);
+gulp.task('default', ['sass', 'images', 'scripts']);
 
 gulp.task('sass', function(done) {
   gulp.src('./scss/ionic.app.scss')
+    .pipe(sourcemaps.init())
     .pipe(sass({
       errLogToConsole: true
     }))
@@ -21,6 +26,7 @@ gulp.task('sass', function(done) {
       keepSpecialComments: 0
     }))
     .pipe(rename({ extname: '.min.css' }))
+    .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('./www/css/'))
     .on('end', done);
 });
@@ -34,9 +40,21 @@ gulp.task('images', function () {
     .pipe(gulp.dest('./www/img/'));
 });
 
+gulp.task('scripts', function() {
+  return gulp.src(['www/js/*.js', 'www/js/**/*.js'])
+    .pipe(sourcemaps.init())
+    .pipe(babel())
+    .pipe(ngAnnotate())
+    .pipe(concat('main.min.js'))
+    .pipe(uglify())
+    .pipe(sourcemaps.write("."))
+    .pipe(gulp.dest('./www/script/'));
+});
+
 gulp.task('watch', function() {
   gulp.watch(['./scss/**/*.scss'], ['sass']);
   gulp.watch(['images/*'], ['images']);
+  gulp.watch(['www/js/*.js', 'www/js/**/*.js'], ['scripts']);
 });
 
 gulp.task('install', ['git-check'], function() {
