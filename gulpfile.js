224654971434e570b815f0eaf7ac6ea826c62c26
kirill.gusyatin@ekreative.com <kirill.gusyatin@ekreative.com>
2016-02-23 10:32:30 +0200

    Remove bulk from platforms

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,4 @@
 var gulp       = require('gulp'),
-    bower      = require('bower'),
     concat     = require('gulp-concat'),
     sass       = require('gulp-sass'),
     minifyCss  = require('gulp-cssnano'),
@@ -17,42 +16,44 @@ var paths = {
 gulp.task('default', ['sass', 'images', 'scripts']);
 
 gulp.task('sass', function(done) {
-  gulp.src('./scss/ionic.app.scss')
-    .pipe(sourcemaps.init())
-    .pipe(sass({
-      errLogToConsole: true
-    }))
-    .pipe(minifyCss({
-      keepSpecialComments: 0
-    }))
-    .pipe(rename({ extname: '.min.css' }))
-    .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest('./www/assets/css/'))
-    .on('end', done);
+    gulp.src('./scss/ionic.app.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass({
+            errLogToConsole: true
+        }))
+        .pipe(minifyCss({
+            keepSpecialComments: 0
+        }))
+        .pipe(rename({
+            extname: '.min.css'
+        }))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('./www/assets/css/'))
+        .on('end', done);
 });
 
-gulp.task('images', function () {
-  gulp.src(['./images/*'])
-    .pipe(imagemin({
-      progressive: true,
-      interlaced: true
-    }))
-    .pipe(gulp.dest('./www/assets/img/'));
+gulp.task('images', function() {
+    gulp.src(['./images/*'])
+        .pipe(imagemin({
+            progressive: true,
+            interlaced: true
+        }))
+        .pipe(gulp.dest('./www/assets/img/'));
 });
 
 gulp.task('scripts', function() {
-  return gulp.src(paths.js)
-    .pipe(sourcemaps.init())
-    .pipe(babel())
-    .pipe(ngAnnotate())
-    .pipe(concat('main.min.js'))
-    .pipe(uglify())
-    .pipe(sourcemaps.write("."))
-    .pipe(gulp.dest('./www/assets/script/'));
+    gulp.src(paths.js)
+        .pipe(sourcemaps.init())
+        .pipe(babel())
+        .pipe(ngAnnotate())
+        .pipe(concat('main.min.js'))
+        .pipe(uglify())
+        .pipe(sourcemaps.write("."))
+        .pipe(gulp.dest('./www/assets/script/'));
 });
 
 gulp.task('watch', function() {
-  gulp.watch(['./scss/**/*.scss'], ['sass']);
-  gulp.watch(['images/*'], ['images']);
-  gulp.watch(paths.js, ['scripts']);
+    gulp.watch(['./scss/**/*.scss'], ['sass']);
+    gulp.watch(['images/*'], ['images']);
+    gulp.watch(paths.js.main, ['scripts']);
 });
---
+++ hooks/after_prepare/030_remove_other_bulk_from_platforms.js
@@ -0,0 +1,40 @@
+#!/usr/bin/env node
+
+/**
+ * After prepare, files are copied to the platforms/ios and platforms/android folders.
+ * Lets clean up some of those files that arent needed with this hook.
+ */
+var fs = require('fs');
+var path = require('path');
+
+var deleteFolderRecursive = function(removePath) {
+  if( fs.existsSync(removePath) ) {
+    fs.readdirSync(removePath).forEach(function(file,index){
+      var curPath = path.join(removePath, file);
+      if(fs.lstatSync(curPath).isDirectory()) { // recurse
+        deleteFolderRecursive(curPath);
+      } else { // delete file
+        fs.unlinkSync(curPath);
+      }
+    });
+    fs.rmdirSync(removePath);
+  }
+};
+
+try {
+  packageJSON = require('../../package.json');
+} catch(ex) {
+  console.log('\nThere was an error fetching your package.json file.')
+  console.log('\nPlease ensure a valid package.json is in the root of this project\n')
+  return;
+}
+
+(packageJSON.cleanFromPlatforms || [] )
+    .forEach(function(toRemove) {
+      var iosPlatformsDir = path.resolve(__dirname, '../../platforms/ios/www/' + toRemove);
+      var androidPlatformsDir = path.resolve(__dirname, '../../platforms/android/assets/www/' + toRemove);
+
+      deleteFolderRecursive(iosPlatformsDir);
+      deleteFolderRecursive(androidPlatformsDir);
+    });
+
